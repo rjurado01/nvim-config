542603c3435361d15bd703504490b9b94894271c
rafael-jurado-gh <rafael.jurado@gesthispania.com>
2025-12-14 10:00:33 +0100

    Diffview custom colors + avante initialization

--- init.lua
+++ init.lua
@@ -2,4 +2,21 @@ require('config.base')
 require('config.lazy')
 require('config.keymaps')
 
-vim.cmd("colorscheme nightfox")
+-- https://github.com/yetone/avante.nvim/issues/2787
+-- vim.defer_fn(function()
+--   vim.cmd("AvanteSwitchProvider gemini")
+--   vim.cmd("AvanteSwitchSelectorProvider snacks")
+-- end, 200)
+
+-- vim.cmd("colorscheme nightfox")
+
+-- vim.api.nvim_set_hl(0, "DiffAdd", { fg = "NONE", ctermfg = "NONE" })
+-- vim.api.nvim_set_hl(0, "DiffDelete", { fg = "NONE", ctermfg = "NONE" })
+-- vim.api.nvim_set_hl(0, "DiffChange", { fg = "NONE", ctermfg = "NONE" })
+-- vim.api.nvim_set_hl(0, "DiffText", { fg = "NONE", ctermfg = "NONE" })
+-- vim.api.nvim_set_hl(0, "SnacksDiffAdd", { bg = "#009900" })
+
+-- vim.api.nvim_set_hl(0, "DiffAdd", { bg = "#263226", fg = "#d7ffaf", blend = 10 })    -- Fondo verde oscuro
+-- vim.api.nvim_set_hl(0, "DiffDelete", { bg = "#322626", fg = "#ffafaf", blend = 10 }) -- Fondo rojo oscuro
+-- vim.api.nvim_set_hl(0, "DiffChange", { bg = "#262632", fg = "#afafff", blend = 10 }) -- Fondo azul oscuro
+-- vim.api.nvim_set_hl(0, "DiffText", { bg = "#323226", fg = "#ffffaf", blend = 10 })   -- Fondo amarillo oscuro
